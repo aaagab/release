1c457e9f6ed3cabdcfa6371c32e975c8c92ae99e
gabaaa <not-set>
2019-03-28 12:47:12 -0500

    In refactoring state for now, I am creating a minimal generic package manager to use until gpm is coded.

--- dev/helpers.py
+++ dev/helpers.py
@@ -3,13 +3,28 @@
 # version: 2.1.0
 # name: release
 # license: MIT
-import sys
-import os
+import os, shlex, sys
 import subprocess
 import modules.shell_helpers.shell_helpers as shell
+import modules.message.message as msg
 
 def get_direpa_root():
-    return subprocess.check_output(shlex.split("git rev-parse --show-toplevel")).decode('utf-8').rstrip()
+    if is_pkg_git():
+        return subprocess.check_output(shlex.split("git rev-parse --show-toplevel")).decode('utf-8').rstrip()
+    else:
+        direpa_current=os.getcwd()
+        direpa_src=os.path.join(direpa_current, "src")
+        if os.path.exists(direpa_src):
+            if is_pkg_git(direpa_src):
+                os.chdir(direpa_src)
+                direpa_root=subprocess.check_output(shlex.split("git rev-parse --show-toplevel")).decode('utf-8').rstrip()
+                os.chdir(direpa_current)
+                return direpa_root
+            else:
+                msg.user_error("In '{}' src exists however".format(os.getcwd()))
+
+        msg.app_error("'{}' is not a git repository".format(os.getcwd()))
+        sys.exit(1)
 
 def append_path_to_ignore_paths(preset, direpa_root):
     for i, path in enumerate(preset["paths"]):
@@ -37,3 +52,10 @@ def is_pkg_git(path=""):
         return False
     else:
         return True
+
+def to_be_coded(text=""):
+    if not text:
+        msg.app_error("To be coded")
+    else:
+        msg.app_error("To be coded: '{}'".format(text))
+    sys.exit(1)
