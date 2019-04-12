8b9acf13d8dc1b87028b89b20b331b2cffa46410
gabaaa <gabaaa.dev@gmail.com>
2019-04-12 10:02:23 -0500

    Bump Release Version 4.1.1

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.1.0
+# version: 4.1.1
 # name: release
 # license: MIT
 
-__version__ = "4.1.0"
+__version__ = "4.1.1"
 
 from .gpkgs import message as msg
 from .modules.options import options as ops
