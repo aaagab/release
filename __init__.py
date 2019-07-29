24c33c32440692dfe1c194e6896c97c98448be78
gabaaa <gabaaa.dev@gmail.com>
2019-07-29 10:32:39 -0500

    Bump Release Version 4.5.0

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 4.4.12
+# version: 4.5.0
 # name: release
 # license: MIT
 
-__version__ = "4.4.12"
+__version__ = "4.5.0"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
