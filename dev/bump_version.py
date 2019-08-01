18867167e308f95035238c24273e14b11c2ee4c3
gabaaa <gabaaa.dev@gmail.com>
2019-08-01 08:43:36 -0500

    Light improvement, I need to refactor refine but the idea is put version only on files at src root

--- dev/bump_version.py
+++ dev/bump_version.py
@@ -16,6 +16,10 @@ def bump_version(version):
     direpa_root=get_direpa_root()
     
     paths=get_paths_to_copy(direpa_root, added_rules=[
+        "config.json",
+        "gpm.json",
+        "modules.json",
+        "upacks.json",
         ".refine",
         "modules/",
         ".pkgs/",
@@ -23,45 +27,47 @@ def bump_version(version):
         "*.db"
     ])
 
+    for elem in ["config/config.json", "config.json", "gpm.json", "modules.json", "upacks.json"]:   
+        filenpa_conf=os.path.join(direpa_root, elem)
+        if os.path.exists(filenpa_conf):
+            conf=Json_config(filenpa_conf)
+            if "version" in conf.data:
+                conf.data["version"]=version
+                conf.set_file_with_data()
+
     for path in paths:
         if os.path.isfile(path):
-            if os.path.basename(path) in ["config.json", "gpm.json", "modules.json", "upacks.json"]:
-                conf=Json_config(path)
-                if "version" in conf.data:
-                    conf.data["version"]=version
-                    conf.set_file_with_data()
-            else:
-                header_version_found=False
-                init_version_found=False
-                data=""
-                try:
-                    with open(path, "r") as f:
-                        line_num=1
-                        for line in f.read().splitlines():
-                            
-                            if header_version_found is False:
-                                if line_num <= 15:
-                                    text=re.match(r"^# version:.*$", line)
-                                    if text:
-                                        header_version_found=True
-                                        data+="# version: {}\n".format(version)
-                                        continue
+            header_version_found=False
+            init_version_found=False
+            data=""
+            try:
+                with open(path, "r") as f:
+                    line_num=1
+                    for line in f.read().splitlines():
+                        
+                        if header_version_found is False:
+                            if line_num <= 15:
+                                text=re.match(r"^# version:.*$", line)
+                                if text:
+                                    header_version_found=True
+                                    data+="# version: {}\n".format(version)
+                                    continue
 
-                            if init_version_found is False:
-                                if os.path.basename(path) == "__init__.py":
-                                    text=re.match(r"^__version__ =.*$", line)
-                                    if text:
-                                        init_version_found=True
-                                        data+="__version__ = \"{}\"\n".format(version)
-                                        continue
+                        if init_version_found is False:
+                            if os.path.basename(path) == "__init__.py":
+                                text=re.match(r"^__version__ =.*$", line)
+                                if text:
+                                    init_version_found=True
+                                    data+="__version__ = \"{}\"\n".format(version)
+                                    continue
 
-                            data+=line+"\n"
-                            line_num+=1
+                        data+=line+"\n"
+                        line_num+=1
 
-                    if header_version_found is True or init_version_found is True:
-                        with open(path, "w") as f:
-                            f.writelines(data)
-                except:
-                    msg.warning("file '{}' is not readable.".format(path))
+                if header_version_found is True or init_version_found is True:
+                    with open(path, "w") as f:
+                        f.writelines(data)
+            except:
+                msg.warning("file '{}' is not readable.".format(path))
     
     msg.success("Bumped version v{}".format(version))
