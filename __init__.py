894c91127e54a194f76b13fa1f493e7c9c6602aa
gabaaa <not-set>
2019-08-02 15:25:25 -0500

    patch

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ from .dev.generate_db import generate_db
 from .dev.import_pkgs import import_pkgs
 from .dev.init import init
 from .dev.ls_repo import ls_repo
-from .dev.remove import remove
+from .dev.remove import removesj 
 from .dev.restore import restore
 from .dev.set_bump_deploy import set_bump_deploy
 from .dev.steps import steps
