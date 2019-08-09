05eb6e5ee6a477623bce98aa505897e00e8dd531
gabaaa <gabaaa.dev@gmail.com>
2019-08-09 16:15:57 -0500

    Bump Release Version 5.1.5

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 5.1.4
+# version: 5.1.5
 # name: release
 # license: MIT
 
-__version__ = "5.1.4"
+__version__ = "5.1.5"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
