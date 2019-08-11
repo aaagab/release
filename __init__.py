e1f1c91b57559d3dc5ffef9c1c607c066cb2d742
gabaaa <not-set>
2019-08-11 14:01:46 -0500

    Bump Release Version 6.2.0

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 6.1.1
+# version: 6.2.0
 # name: release
 # license: MIT
 
-__version__ = "6.1.1"
+__version__ = "6.2.0"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
