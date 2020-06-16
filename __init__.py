b9154a9946f410d30cc36bdb697c25e7d87d4ddc
gabaaa <not-set>
2020-06-15 22:40:41 -0500

    Bump Release Version 8.0.0

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 7.2.0
+# version: 8.0.0
 # name: release
 # license: MIT
 
-__version__ = "7.2.0"
+__version__ = "8.0.0"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
