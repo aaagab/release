d515729e494cbcf95c7ecccc6e6bb46b1c83c303
gabaaa <gabaaa.dev@gmail.com>
2020-10-14 15:17:54 -0500

    modified steps

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 9.5.0
+# version: 9.5.1
 # name: release
 # license: MIT
 
-__version__ = "9.5.0"
+__version__ = "9.5.1"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
