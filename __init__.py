8b996023b06af4f2e1ae674d375fffedcfd5ecf9
gabaaa <gabaaa.dev@gmail.com>
2020-10-14 14:56:21 -0500

    modified windows path

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 9.4.2
+# version: 9.5.0
 # name: release
 # license: MIT
 
-__version__ = "9.4.2"
+__version__ = "9.5.0"
 
 from .dev.bump_version import bump_version
 from .dev.check_repo import check_repo
