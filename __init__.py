1b03aa35947e978e8ca0a21b3a2e291ebdcab640
gabaaa <gabaaa.dev@gmail.com>
2020-11-27 12:33:28 -0600

    added --filter-rules for import

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 12.3.0
+# version: 12.4.0
 # name: release
 # license: MIT
 
-__version__ = "12.3.0"
+__version__ = "12.4.0"
 
 from .dev.bump_version import bump_version
 from .dev.check_pkg_integrity import check_pkg_integrity
