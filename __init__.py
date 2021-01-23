d9a1e3dbd23d9826ee898b808d97ca6fa4d96fba
gabaaa <gabaaa.dev@gmail.com>
2021-01-22 18:28:39 -0600

    fix

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 12.4.3
+# version: 12.4.4
 # name: release
 # license: MIT
 
-__version__ = "12.4.3"
+__version__ = "12.4.4"
 
 from .dev.bump_version import bump_version
 from .dev.check_pkg_integrity import check_pkg_integrity
