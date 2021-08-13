6342e6622196dd93d051146e0b13fc53ff59d348
gabaaa <gabaaa.dev@gmail.com>
2021-08-13 16:24:07 -0500

    Removed linux launchers for scriptjob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # author: Gabriel Auger
-# version: 13.3.3
+# version: 13.4.0
 # name: release
 # license: MIT
 
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     from pprint import pprint
     import os
     import sys
+
     direpa_script_parent=os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     module_name=os.path.basename(os.path.dirname(os.path.realpath(__file__)))
     sys.path.insert(0, direpa_script_parent)
     pkg = importlib.import_module(module_name)
     del sys.path[0]
-    
+
     args, dy_app=pkg.Options(filenpa_app="gpm.json", filenpa_args="config/options.json").get_argsns_dy_app()
     dy_default_direpas=dict(
         bin=os.path.join(os.path.expanduser("~"), "fty", "bin"),
